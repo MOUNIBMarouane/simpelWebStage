d1758c1611795b95a7ab32e81f25ba3dfcc33241
MOUNIBMarouane <marouan.mounib33@gmail.com>
2025-02-21 16:20:22 +0100

    add config for backend connection

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,13 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(),react()],
+    server: {
+    proxy: {
+      '/api': {
+        target: 'http://192.168.1.85:5204',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })
-
